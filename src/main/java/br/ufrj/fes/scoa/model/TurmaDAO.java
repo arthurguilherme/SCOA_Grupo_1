1a02f7cebac1fd56240bf963f0a6a7caa5d4d8b7
diegocf1 <diegocf1@users.noreply.github.com>
2019-11-08 02:12:43 -0300

    Bump v0.0.4

--- src/main/java/br/ufrj/fes/scoa/model/TurmaDAO.java
+++ src/main/java/br/ufrj/fes/scoa/model/TurmaDAO.java
@@ -2,6 +2,7 @@ package br.ufrj.fes.scoa.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import br.ufrj.fes.scoa.ConexaoFactory;
 
@@ -9,19 +10,32 @@ public class TurmaDAO {
 	public static void cadastrar(Turma turma) throws Exception {
 		String query = "INSERT INTO turma (codigo_disciplina, vagas, matricula_professor, qtd_aulas) VALUES(?, ?, ?, ?)";
 		Connection conexao = null;
-		PreparedStatement ps = null;		
 		PreparedStatement ps2 = null;
-		PreparedStatement ps3 = null;		
-		//CallableStatement cs = null;
-		try (conexao = ConexaoFactory.criarConexao();			
-			ps2 = conexao.prepareStatement(query)) {
-			ps2.setString(1, turma.getCodigo());
-			ps2.setString(2, turma.getVagas());
+		try {
+			conexao = ConexaoFactory.criarConexao();			
+			ps2 = conexao.prepareStatement(query);
+			ps2.setInt(1, turma.getCodigo());
+			ps2.setInt(2, turma.getVagas());
 			ps2.setInt(3, turma.getProfessor().getMatricula());
 			ps2.setInt(4, turma.getAulas());
 			ps2.execute();
 
-		} 
+		} finally {
+			try {
+        
+                if (ps2 != null) {
+                    ps2.close();
+                }
+            } catch (SQLException se) {}
+			
+			
+			try {
+                if (conexao != null) {
+                    conexao.close();
+                }
+            } catch (SQLException se) {}
+			
+		}
 		
 	}
 }
